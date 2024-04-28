3e2136afca943351d5b2e70b956d407a139eeef0
muhammadfahid12 <ranafahadirshad@gmail.com>
2024-04-29 00:40:26 +0500

    completed pattern 9-12

--- Patterns/patterns.cpp
+++ Patterns/patterns.cpp
@@ -88,7 +88,7 @@ void pattern8(int n)
         int x = 2;
         x = x - i;
         // for space
-        for (int s = 0; s < i ; s++)
+        for (int s = 0; s < i; s++)
         {
             cout << " ";
         }
@@ -100,16 +100,124 @@ void pattern8(int n)
             cout << "*";
         }
 
-
         cout << endl;
 
         // cout << "the value of x is "<< x;
     }
 }
 
+// pattern 9 can be formed by combining pattern 7 and pattern 8.
+/*pattern7(5);
+pattern8(5);*/
+
+// pattern 10
+
+void pattern10(int n)
+{
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    for (int i = 0; i < n; i++)
+    {
+
+        for (int k = 0; k < n - i - 1; k++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+
+void pattern10Optimized(int n)
+{
+
+    int x = 1;
+    for (int i = 1; i < 2 * n; i++)
+    {
+        // we added if condition that if our row number become greater than our n; we'll do
+        //  in this example, we used x and starsCount as new variable to solve problem
+        int starsCount = i;
+        /*if(i > n)
+        {
+            starsCount = starsCount - 2*x;
+            x++;
+        }
+        */
+
+        // another alternative for line 146 to 151 is
+
+        if (i > n)
+        {
+            starsCount = 2 * n - i;
+        }
+        for (int j = 0; j < starsCount; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+
+void pattern11(int n)
+{
+
+    for (int i = 1; i <= n; i++)
+    {
+        int start = 0;
+
+        if (i % 2 == 0)
+            start = 1;
+
+        for (int j = 1; j <= i; j++)
+        {
+
+            start = 1 - start;
+
+            cout << start << " ";
+        }
+        cout << endl;
+    }
+}
+
+void pattern12(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        //first assembly
+
+        for (int j = 1; j <= i; j++)
+        {
+            cout << j;
+        }
+
+        //spaces
+
+        for(int s = 1;s <=2*n - 2*i;s++ )
+        {
+            cout << " ";
+        }
+
+        //second assembly
+
+         for (int k = i; k >= 1; k--)
+        {
+            cout << k;
+        }
+
+
+        cout << endl;
+    }
+}
+
 int main()
 {
-    pattern8(5);
+    pattern12(4);
 
     return 0;
 }
